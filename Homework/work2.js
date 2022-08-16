089c621b854002970a4727353c7f01c768200780
RASARUS <seonbi1998@naver.com>
2022-08-16 17:07:56 +0900

    1day 1commit

--- Homework/work2.js
+++ Homework/work2.js
@@ -5,5 +5,8 @@ for (i = 0; i < 3; i++) {
 
 function titlePrice(a) {
     document.querySelectorAll('h5')[a].innerHTML = products[a].title
-    document.querySelectorAll('p')[a].innerHTML = products[a].price
+    document.querySelectorAll('p')[a].innerHTML = '가격 : '+products[a].price
 }
+//문자중간에 변수집어넣기 '백틱'
+var b= 'b';
+console.log(`a${b}a`)
