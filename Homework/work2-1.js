3c97ad074262e5d0ba0ac0af6bd111b7ca858110
Anakin787 <seonbi1998@naver.com>
2022-09-06 16:06:08 +0900

    1day1commit

--- Homework/work2-1.js
+++ Homework/work2-1.js
@@ -1,39 +1,49 @@
-products.forEach((a) => {
-    $('.row').eq(0).append(`<div class="col-sm-4">
+
+$('.btn').eq(0).on('click', function () {
+    products.sort(function (a, b) {
+        if (a.title < b.title) return 1;
+        if (a.title > b.title) return -1;
+        if (a.title === b.title) return 0;
+    })
+    $('.row').html('')
+    products.forEach((a) => {
+        $('.row').eq(0).append(`<div class="col-sm-4">
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${a.title}</h5>
     <p>가격 : ${a.price}</p>
   </div>`)
+    })
+    i=0
 })
 
-var i=0
-$('.btn').on('click',function(){
-    if(i==0){
+
+var i = 0
+$('.btn').eq(1).on('click', function () {
+    if (i == 0) {
         $.get('https://codingapple1.github.io/js/more1.json')
-        .done(function(data){
-            data.forEach((b) => {
-                $('.row').eq(0).append(`<div class="col-sm-4">
+            .done(function (data) {
+                newdata = data.filter(a=> a.price <= 60000)
+                newdata.forEach((b) => {
+                    $('.row').eq(0).append(`<div class="col-sm-4">
                 <img src="https://via.placeholder.com/600" class="w-100">
                 <h5>${b.title}</h5>
                 <p>가격 : ${b.price}</p>
               </div>`)
+                })
             })
-        })
         i++
     }
-    else if(i==1){
+    else if (i == 1) {
         $.get('https://codingapple1.github.io/js/more2.json')
-        .done(function(data){
-            data.sort(function(a,b){
-                return a.price-b.price
-            })
-            data.forEach((b) => {
-                $('.row').eq(0).append(`<div class="col-sm-4">
+            .done(function (data) {
+                newdata = data.filter(a => a.price <= 60000)
+                newdata.forEach((b) => {
+                    $('.row').eq(0).append(`<div class="col-sm-4">
                 <img src="https://via.placeholder.com/600" class="w-100">
                 <h5>${b.title}</h5>
                 <p>가격 : ${b.price}</p>
               </div>`)
+                })
             })
-        })
     }
 })
