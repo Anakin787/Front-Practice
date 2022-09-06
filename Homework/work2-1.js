9ec1581c1dec136b63700ebe120b92db4614df1d
Anakin787 <seonbi1998@naver.com>
2022-09-06 18:57:26 +0900

    1day1commit

--- Homework/work2-1.js
+++ Homework/work2-1.js
@@ -1,20 +1,39 @@
+$('#btn').on('submit', function () {
+    var value = $('#num').val();
 
-$('.btn').eq(0).on('click', function () {
-    products.sort(function (a, b) {
-        if (a.title < b.title) return 1;
-        if (a.title > b.title) return -1;
-        if (a.title === b.title) return 0;
-    })
+    printValue();
     $('.row').html('')
-    products.forEach((a) => {
-        $('.row').eq(0).append(`<div class="col-sm-4">
+
+    inp = products.filter(v => v.price <= value)
+
+    inp.forEach((a) => {
+        $('.row').append(`<div class="col-sm-4">
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${a.title}</h5>
     <p>가격 : ${a.price}</p>
   </div>`)
     })
-    i=0
 })
+// function printValue(){
+//     var value = $('#num').val();
+// }
+
+// $('.btn').eq(0).on('click', function () {
+//     products.sort(function (a, b) {
+//         if (a.title < b.title) return 1;
+//         if (a.title > b.title) return -1;
+//         if (a.title === b.title) return 0;
+//     })
+//     $('.row').html('')
+//     products.forEach((a) => {
+//         $('.row').eq(0).append(`<div class="col-sm-4">
+//     <img src="https://via.placeholder.com/600" class="w-100">
+//     <h5>${a.title}</h5>
+//     <p>가격 : ${a.price}</p>
+//   </div>`)
+//     })
+//     i = 0
+// })
 
 
 var i = 0
@@ -22,7 +41,7 @@ $('.btn').eq(1).on('click', function () {
     if (i == 0) {
         $.get('https://codingapple1.github.io/js/more1.json')
             .done(function (data) {
-                newdata = data.filter(a=> a.price <= 60000)
+                newdata = data.filter(a => a.price <= 60000)
                 newdata.forEach((b) => {
                     $('.row').eq(0).append(`<div class="col-sm-4">
                 <img src="https://via.placeholder.com/600" class="w-100">
