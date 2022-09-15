2fcf04c9fa2e3aa54008539e5148e52deed5e689
Anakin787 <seonbi1998@naver.com>
2022-09-15 16:02:24 +0900

    1day1commit

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    컨테이너에요
+    <post/>
+    <post/>
+    <post/>
+  </div>
+</template>
+
+<script>
+  import Post from './Post.vue'
+
+export default {
+ components:{
+  Post:Post
+ }
+}
+</script>
+
+<style>
+
+</style>
