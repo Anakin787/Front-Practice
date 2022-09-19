fb0b8f805d085b304e622c9359222c1110b5a44c
Anakin787 <seonbi1998@naver.com>
2022-09-19 18:55:41 +0900

    1day1commit

--- ES6/es.js
+++ ES6/es.js
@@ -1,8 +1,20 @@
-var 사람 = {
-    name: '손흥민',
-    sayHi:function(){
-        return console.log(`안녕 나는 ${this.name}`)
+// var 사람 = {
+//     name: '손흥민',
+//     sayHi: function () {
+//         return console.log(`안녕 나는 ${this.name}`)
+//     }
+// }
+// 사람.sayHi();
+
+var 자료 = {
+    data: [1, 2, 3, 4, 5],
+    전부더하기: function() {
+        this.data.forEach((a,i) => {
+            // a=a+this.data[i+1]
+            this.data[i]+=this.data[i+1]
+            console.log(this.data[i])
+            // console.log(a)
+        });
     }
-  }
-  
-  사람.sayHi();
+}
+자료.전부더하기()
